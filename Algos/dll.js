bea30ec888f7ff39b8d2972f560713ab7b45920f
Cody Thaller <stevencthaller@gmail.com>
2020-06-18 08:37:23 -0700

    Dingly Linked Lists 6/18

---
+++ Algos/dll.js
@@ -0,0 +1,40 @@
+class DLNode {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
+class DLList {
+    constructor(){
+        this.head = null;
+    }
+
+    // Write a method that will return whether or not the 
+    // doubly linked list is empty.
+    isEmpty(){
+        
+    }
+
+    // Write a method that will add a new node to the end of our
+    // doubly linked list.
+    append(value) {
+
+    }
+
+    // Write a method that will add a new node to the beginning of
+    // our doubly linked list.
+    prepend(value) {
+
+    }
+
+    // Write a method that will add a new node at a given "index" of our
+    // doubly linked list
+
+    // NOTE! Test for if the index given is out of range!
+    push(value, index){
+
+    }
+}
