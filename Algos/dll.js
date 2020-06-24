1c33d70237ed227826b60a8421619805247778a6
Cody Thaller <stevencthaller@gmail.com>
2020-06-24 08:59:12 -0700

    Algos for 6/24

--- Algos/dll.js
+++ Algos/dll.js
@@ -15,19 +15,36 @@ class DLList {
     // Write a method that will return whether or not the 
     // doubly linked list is empty.
     isEmpty(){
-        
+        return this.head === null;
     }
 
     // Write a method that will add a new node to the end of our
     // doubly linked list.
     append(value) {
-
+        if(this.isEmpty()){
+            this.head = new DLNode(value);
+            return this;
+        }
+        let runner = this.head;
+        while(runner.next != null){
+            runner = runner.next;
+        }
+        let newNode = new DLNode(value);
+        runner.next = newNode;
+        newNode.prev = runner;
+        return this;
     }
 
     // Write a method that will add a new node to the beginning of
     // our doubly linked list.
     prepend(value) {
-
+        let newNode = new DLNode(value);
+        newNode.next = this.head;
+        if(this.head != null){
+            this.head.prev = newNode;
+        }
+        this.head = newNode;
+        return this;
     }
 
     // Write a method that will add a new node at a given "index" of our
@@ -35,6 +52,54 @@ class DLList {
 
     // NOTE! Test for if the index given is out of range!
     push(value, index){
+        if(index < 0) {
+            console.log("C'mon, that's not an index.");
+            return this;
+        }
+        if(this.isEmpty()) {
+            if(index > 0){
+                console.log("No can do grizzly bear.");
+                return this;
+            }
+            else {
+                this.head = new DLNode(value);
+                return this;
+            }
+        }
+        if(index == 0){
+            this.prepend(value);
+            return this;
+        }
+        
+        let runner = this.head;
+        let newNode = new DLNode(value);
+        for(var i = 1; i < index; i++){
+            if(runner == null){
+                console.log("No");
+                return this;
+            }
+            runner = runner.next;
+        }
+        newNode.next = runner.next;
+        newNode.prev = runner;
+        runner.next = newNode;
+        newNode.next.prev = newNode;
+        return this;
+    }
 
+    printList(){
+        if(this.isEmpty()){
+            console.log("List is empty.")
+            return this;
+        }
+        let toPrint = "<- ";
+        let runner = this.head;
+        while(runner.next != null) {
+            toPrint += `${runner.value} <-> `;
+            runner = runner.next;
+        }
+        toPrint += `${runner.value} ->`;
+        console.log(toPrint);
+        return this;
     }
 }
---
+++ Algos/trees.js
@@ -0,0 +1,33 @@
+class BSNode {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BSTree {
+    constructor(){
+        this.root = null;
+    }
+
+    // I don't think I need to say anything for this one lol
+    isEmpty(){
+
+    }
+
+    // Write a method that will insert a new node into the BST
+    insert(value){
+
+    }
+
+    // Write a method that will return the largest element in the BST
+    max(){
+
+    }
+
+    // Write a method that will return the smallest element in the BST
+    min(){
+
+    }
+}
